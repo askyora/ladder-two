aba5f2f43d927d30b3dd0ee753dbe7a38013ec3b
Yohan Ranasinghe <yohanra.io@gmail.com>
2023-11-02 16:55:46 +0530

    Project setup

---
+++ ladder-api/src/main/java/com/yora/ladder/LadderApiApplication.java
@@ -0,0 +1,13 @@
+package com.yora.ladder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadderApiApplication.class, args);
+	}
+
+}
