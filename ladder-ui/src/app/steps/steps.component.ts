f6b1ae5c6921bfa18d52d33e877680d235407d3e
Yohan Ranasinghe <yohanra.io@gmail.com>
2023-11-17 14:53:31 +0530

    list of steps

--- ladder-ui/src/app/steps/steps.component.ts
+++ ladder-ui/src/app/steps/steps.component.ts
@@ -18,5 +18,50 @@ export class StepsComponent {
     parentId: -1,
   };
 
+  steps: Step[] = [
+    {
+      id: 1,
+      name: 'Universe',
+      address: "universe",
+      description: 'Universe level settings',
+      clientId: 1,
+      inheritable: true,
+      overridable: true,
+      parentId: -1,
+    },
+    {
+      id: 2,
+      name: 'Earth',
+      address: "earth",
+      description: 'Earth level settings',
+      clientId: 1,
+      inheritable: true,
+      overridable: true,
+      parentId: 1,
+    },
+
+    {
+      id: 3,
+      name: 'APEC',
+      address: "earth.apec",
+      description: 'Apec level settings',
+      clientId: 1,
+      inheritable: true,
+      overridable: true,
+      parentId: 2,
+    },
+    {
+      id: 4,
+      name: 'US',
+      address: "earth.us",
+      description: 'Apec level settings',
+      clientId: 1,
+      inheritable: true,
+      overridable: true,
+      parentId: 2,
+    }
+
+  ];
+
 
 }
